66c3fb2ac56f8fe0f32c4f44515a8b7efe6307cc
mholtkamp <mholt012@gmail.com>
2025-06-02 22:08:25 -0400

    Handle gamepad and mouse controls for menu

--- Bomber/Scripts/MainMenu.lua
+++ Bomber/Scripts/MainMenu.lua
@@ -8,6 +8,7 @@ function MainMenu:Start()
     self.optSolo = self:FindChild('OptSolo', true)
     self.optCreate = self:FindChild('OptCreateNet', true)
     self.optJoin = self:FindChild('OptJoinNet', true)
+    self.buttons = { self.optSolo, self.optCreate, self.optJoin }
 
     -- OnActivated will pass a self param if needed
     -- You can make a single script that implements an OnActivated function instead of
@@ -17,17 +18,45 @@ function MainMenu:Start()
     self.optCreate.OnActivated = function() GameState:StartNetMatch() end
     self.optJoin.OnActivated = function() GameState:JoinNetMatch() end
 
+    for i=1, #self.buttons do 
+        self.buttons[i]:ConnectSignal("Hovered", self, function() self.selIndex = i end)
+    end
+
+    self.optSolo:SetState(ButtonState.Hovered)
+    self.selIndex = 1
 end
 
 function MainMenu:Tick(deltaTime)
 
+    -- Button navigation and activation
+    if (Input.IsGamepadPressed(Gamepad.Down) or Input.GetGamepadAxis(Gamepad.AxisLY) < -0.5) then
+        self.selIndex = self.selIndex + 1
+    end
+
+       if (Input.IsGamepadPressed(Gamepad.Up) or Input.GetGamepadAxis(Gamepad.AxisLY) > 0.5) then
+        self.selIndex = self.selIndex - 1
+    end
+
+    self.selIndex = Math.Clamp(self.selIndex, 1, #self.buttons)
+
+    if (self.buttons[self.selIndex]:GetState() == ButtonState.Normal) then
+        self.buttons[self.selIndex]:SetState(ButtonState.Hovered)
+    end
+
+    if (Input.IsGamepadPressed(Gamepad.A)) then
+        self.buttons[self.selIndex]:Activate()
+    end
+
+    -- Start begins a solo match
     if (Input.IsGamepadButtonJustDown(Gamepad.Start)) then
         Log.Debug('Starting Solo Match')
         GameState:StartSoloMatch()
     end
 
+    -- Select quits the game
     if (Input.IsGamepadPressed(Gamepad.Select) or 
             (Input.IsGamepadDown(Gamepad.L1) and Input.IsGamepadDown(Gamepad.R1))) then
         Engine.Quit()
     end
+
 end
