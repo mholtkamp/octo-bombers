9213c4c7c339007c46a475efcd39b1ab01a2c9ac
mholtkamp <mholt012@gmail.com>
2025-06-02 21:34:03 -0400

    Add buttons main menu

--- Bomber/Scripts/MainMenu.lua
+++ Bomber/Scripts/MainMenu.lua
@@ -3,20 +3,19 @@ MainMenu =
 
 }
 
-
 function MainMenu:Start()
 
     self.optSolo = self:FindChild('OptSolo', true)
     self.optCreate = self:FindChild('OptCreateNet', true)
     self.optJoin = self:FindChild('OptJoinNet', true)
 
-    local soloFunc = function() GameState:StartSoloMatch() end
-    local createFunc = function() GameState:StartNetMatch() end
-    local joinFunc = function() GameState:JoinNetMatch() end
-
-    self.optSolo:SetActivateFunc(soloFunc)
-    self.optCreate:SetActivateFunc(createFunc)
-    self.optJoin:SetActivateFunc(joinFunc)
+    -- OnActivated will pass a self param if needed
+    -- You can make a single script that implements an OnActivated function instead of
+    -- setting the function externally like we are doing here.
+    -- And you can also handle "Activated", "Hovered", "Pressed" signals if you prefer.
+    self.optSolo.OnActivated = function() GameState:StartSoloMatch() end
+    self.optCreate.OnActivated = function() GameState:StartNetMatch() end
+    self.optJoin.OnActivated = function() GameState:JoinNetMatch() end
 
 end
 
